629438f4973a09347c1ca06d1ae9b403990262e2
Frederic Boudon <frederic.boudon@cirad.fr>
2021-03-05 16:05:33 +0100

    improve analysis with triangles based analysis for caribu

--- theoretical/display_theozeta.py
+++ theoretical/display_theozeta.py
@@ -1,5 +1,6 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.markers import MarkerStyle
 from matplotlib import cm
 import numpy as np
 
@@ -8,12 +9,12 @@ cmap = cm.get_cmap('summer')
 def plot_disks(values, ax, figcolorbar = None):
     m = len(values)
     norm = mpl.colors.Normalize(vmin=0,vmax=max(1,max(values)))
-    ax.plot([0,0],[-2,-1.1],c='brown',linewidth=40)
     for i, v in enumerate(values):
-        if not np.isnan(v):
-          r = 1-(i/(m-1))
-          circle = plt.Circle((0, 0), r, color= cmap(norm(v)))
-          ax.add_patch(circle)
+        color = cmap(norm(v)) if not np.isnan(v) else 'black'
+        r = 1-(i/m)
+        circle = plt.Circle((0, 0), r, facecolor=color , edgecolor='black')
+        ax.add_patch(circle)
+    ax.plot([0,0],[-2,-1.1],c='brown',linewidth=40)
     ax.set_xlim(-1.2,1.2)
     ax.set_ylim(-1.2,1.2)
     if figcolorbar:
@@ -54,4 +55,84 @@ def plot_zeta_analysis(par, r, fr, par_sky = None, r_sky = None, fr_sky = None):
     plot_disks(fr, ax6, fig)
     ax6.set_title('FR')
     #ax4.plot(np.full((m), 0.5),np.full((m), 0.5), marker='o', markersize=np.arange(1,m+1,1))
+    plt.show()
+
+layershift = 1000000
+
+
+def sort_values(values):
+    msphere = max([(tid // layershift) for tid in values.keys()])
+    svalues = [[] for i in range(0,msphere)]
+    for tid, v in values.items():
+        svalues[(tid // layershift)-1].append(v)
+    return svalues
+
+
+def compute_angles(sc):
+    from openalea.plantgl.all import Vector3
+    from math import degrees
+    svalues = {}
+    for sh in sc:
+        pcenter = sh.geometry.pointList.getCenter()
+        v = Vector3.Spherical(pcenter)
+        svalues[sh.id] = degrees(v.phi)
+    return svalues
+
+#jet = cm.get_cmap('jet')
+
+def plot_zeta_analysis_per_triangles(par, r, fr, sc):
+    #ranks = range(1,len(par)+1)
+    angles = sort_values(compute_angles(sc))
+    #norm = mpl.colors.Normalize(vmin=0,vmax=180)
+
+    sr = sort_values(r)
+    sfr = sort_values(fr)
+    spar = sort_values(par)
+    zetas = [np.array(ri)/np.array(fri) for ri,fri in zip(sr,sfr)]
+    fig, axes = plt.subplots(2, 3, figsize=(15, 8))
+    (ax1, ax2, ax3), (ax4, ax5, ax6) = axes
+
+    for i, (zetai, pari) in enumerate(zip(zetas,spar)):
+        ax1.scatter(pari,zetai,label = 'layer '+str(i+1))
+
+    ax1.set_xlim(0,max(1,max(par.values())))
+    ax1.set_ylim(0,max(1,max([max(zetai) if len(zetai) > 0 else 0 for zetai in zetas])))
+    ax1.set_ylabel('Zeta')
+    ax1.set_xlabel('PAR')
+    ax1.legend()
+    ax1.set_title('Zeta')
+
+    for i, (zetai, pari, anglesi) in enumerate(zip(zetas,spar,angles)):
+        ax2.scatter(pari,zetai, c=anglesi , cmap='jet', vmin=0, vmax=180) #, marker= MarkerStyle.filled_markers[i])
+        
+    ax2.set_xlim(0,max(1,max(par.values())))
+    ax2.set_ylim(0,max(1,max([max(zetai) if len(zetai) > 0 else 0 for zetai in zetas])))
+    #ax2.legend()
+    ax2.set_ylabel('Zeta')
+    ax2.set_xlabel('PAR')
+    ax2.set_title('Zeta')
+
+    for i, (zetai, anglesi) in enumerate(zip(zetas,angles)):
+        ax3.scatter(anglesi,zetai,label = 'layer '+str(i+1)) #, marker= MarkerStyle.filled_markers[i])
+    ax3.set_xlim(0,180)
+    ax3.set_ylim(0,max(1,max([max(zetai) if len(zetai) > 0 else 0 for zetai in zetas])))
+    ax3.legend()
+    ax3.set_ylabel('Zeta')
+    ax3.set_xlabel('Elevation')
+    ax3.set_title('Zeta')
+
+    #ax4.set_title('PAR')
+    ax4.boxplot(spar)
+    ax4.set_ylabel('PAR')
+    ax4.set_xlabel('Layer')
+    
+    #ax5.set_title('R')
+    ax5.boxplot(sr)
+    ax5.set_ylabel('R')
+    ax5.set_xlabel('Layer')
+
+    #ax6.set_title('FR')
+    ax6.boxplot(sfr)
+    ax6.set_ylabel('FR')
+    ax6.set_xlabel('Layer')
     plt.show()
--- theoretical/theozeta_caribu.py
+++ theoretical/theozeta_caribu.py
@@ -5,6 +5,7 @@ from alinea.caribu.sky_tools import GenSky, GetLight, Gensun, GetLightsSun
 import time, datetime
 from display_theozeta import *
 from random import uniform
+from math import sqrt
 
 
 tR, tFR, tPAR = 0.007, 0.35, 0.025 
@@ -13,10 +14,17 @@ rR, rFR, rPAR = 0.06, 0.435, 0.08
 #tR, tFR, tPAR = 0.01, 0.4, 0.01
 #rR, rFR, rPAR = 0.01, 0.4, 0.01
 
+layershift = 1000000
+
+
 def compute_scene(gapfraction = 0, 
                       tR = tR, tFR = tFR, tPAR = tPAR,
                       rR = rR, rFR = rFR, rPAR = rPAR, 
-                      nblayers = 5):
+                      nblayers = 5,
+                      discretization = 8,
+                      pertriangles = False):
+    assert 0 <= gapfraction <= 1
+
     R = (tR, tR)  # (0.10, 0.07, 0.10, 0.07)  # Combes et al
     FR = (tFR, tFR)  # (0.41, 0.43, 0.41, 0.43)
     PAR = (tPAR, tPAR)  # (0.41, 0.43, 0.41, 0.43)
@@ -26,45 +34,59 @@ def compute_scene(gapfraction = 0,
     # strategie des m spheres concentriques
     scene = Scene()
     t = Tesselator()
-    invgf = 1 - gapfraction
+    sf = sqrt(1-gapfraction)
+    invgf = (1 - sf)/2
 
     for lid in range(1,nblayers+1):
-        sp = Sphere(nblayers - lid)
-        if gapfraction > 0:
+        sp = Sphere(nblayers+1 - lid, discretization, discretization)
+        if pertriangles or gapfraction > 0 :
             sp.apply(t)
             triangleset = t.result
             points = triangleset.pointList
             pts = []
             indices = []
             for i, idx in enumerate(triangleset.indexList):
-                p0 = points[idx[0]]
-                p1 = points[idx[1]]
-                p2 = points[idx[2]]
+                p0 = Vector3(points[idx[0]])
+                p1 = Vector3(points[idx[1]])
+                p2 = Vector3(points[idx[2]])
                 v1 = p1-p0
                 v2 = p2-p0
-                p0b = p0 + v1*uniform(0, invgf) + v2*uniform(0, invgf)
-                p1b = p0b + v1 * gapfraction
-                p2b = p0b + v2 * gapfraction
-                pts += [p0b, p1b, p2b]
-                indices.append([3*i,3*i+1,3*i+2])
-            sp = TriangleSet(pts, indices)
-        scene.add(Shape(sp, id = lid))
-        opt['FR'][lid] = FR
-        opt['R'][lid] = R
-        opt['PAR'][lid] = PAR
+                u = uniform(0, invgf)
+                p0b = p0 + v1*u + v2*uniform(0,invgf-u)
+                p1b = p0b + (v1 * sf)
+                p2b = p0b + (v2 * sf)
+                tpts = [p0b, p1b, p2b]
+                if pertriangles:
+                    sh = Shape(TriangleSet(tpts, [list(range(3))]), id = lid*layershift+i )
+                    scene.add(sh)
+                    opt['FR'][lid*layershift+i] = FR
+                    opt['R'][lid*layershift+i] = R
+                    opt['PAR'][lid*layershift+i] = PAR
+                else:
+                    pts += tpts
+                    indices.append([3*i,3*i+1,3*i+2])
+            if not pertriangles:
+                sp = TriangleSet(pts, indices)
+        if not pertriangles:
+            scene.add(Shape(sp, id = lid))
+            opt['FR'][lid] = FR
+            opt['R'][lid] = R
+            opt['PAR'][lid] = PAR
 
     return scene, opt
 
 def compute_zeta_caribu(gapfraction = 0, 
                       tR = tR, tFR = tFR, tPAR = tPAR,
                       rR = rR, rFR = rFR, rPAR = rPAR, 
-                      nblayers = 5, resultingscene = ''):
+                      nblayers = 5, 
+                      discretization = 8,
+                      resultingscene = ''):
     assert (tR+rR) <= 1
     assert (tFR+rFR) <= 1
     assert (tPAR+rPAR) <= 1
 
     ranks = range(1,nblayers+1)
-    scene, opt = compute_scene(gapfraction, tR, tFR, tPAR, rR, rFR, rPAR, nblayers)
+    scene, opt = compute_scene(gapfraction, tR, tFR, tPAR, rR, rFR, rPAR, nblayers, discretization)
 
     # Soleil vertical
     soleil = [(1.0, (0., -0.0, -1))]
@@ -86,32 +108,92 @@ def compute_zeta_caribu(gapfraction = 0,
     par = np.array([aggregated['PAR']['Ei'][lid] for lid in ranks])
     r = np.array([aggregated['R']['Ei'][lid] for lid in ranks])
     fr = np.array([aggregated['FR']['Ei'][lid] for lid in ranks])
-    res = (par, r, fr, None, None, None)
     if resultingscene:
         sc, v = cc_scene.plot(raw[resultingscene]['Ei'], display=False)
-        return par, r, fr, None, None, None, sc
-    return par, r, fr, None, None, None, None
+        nsc = Scene()
+        for sh in sc:
+            geom = sh.geometry
+            for idtr,idx in enumerate(geom.indexList):
+                c = geom.colorList[idtr]
+                nsc.add(Shape(TriangleSet([geom.pointList[i] for i in idx],[list(range(3))]), Material(Color3(c.red,c.green,c.blue)),sh.id))
+        return par, r, fr, nsc
+    return par, r, fr, None
+
+
+def compute_zeta_caribu_per_triangles(gapfraction = 0, 
+                      tR = tR, tFR = tFR, tPAR = tPAR,
+                      rR = rR, rFR = rFR, rPAR = rPAR, 
+                      nblayers = 5, 
+                      discretization = 8,
+                      resultingscene = ''):
+    assert (tR+rR) <= 1
+    assert (tFR+rFR) <= 1
+    assert (tPAR+rPAR) <= 1
+
+    scene, opt = compute_scene(gapfraction, tR, tFR, tPAR, rR, rFR, rPAR, nblayers, discretization, pertriangles = True)
+
+    # Soleil vertical
+    soleil = [(1.0, (0., -0.0, -1))]
+
+    # Viewer.display(s_prov)
+    cc_scene = CaribuScene(scene=scene, opt=opt, light=soleil)
+
+    current_time_of_the_system = time.time()
+    raw, aggregated = cc_scene.run(direct=False, 
+                                        infinite=False,
+                                        split_face=False)  # ,sensors=dico_capt)#,screen_resolution=12000)
+    execution_time = int(time.time() - current_time_of_the_system)
+
+
+    par = aggregated['PAR']['Ei']
+    r = aggregated['R']['Ei']
+    fr = aggregated['FR']['Ei']
+    if resultingscene:
+        sc, v = cc_scene.plot(raw[resultingscene]['Ei'], display=False)
+        nsc = Scene()
+        for sh in sc:
+            geom = sh.geometry
+            for idtr,idx in enumerate(geom.indexList):
+                c = geom.colorList[idtr]
+                nsc.add(Shape(TriangleSet([geom.pointList[i] for i in idx],[list(range(3))]), Material(Color3(c.red,c.green,c.blue)),sh.id))
+        return par, r, fr, nsc, scene
+    return par, r, fr, None
 
 
 
 def plot_zeta_caribu(gapfraction = 0, 
                       tR = tR, tFR = tFR, tPAR = tPAR,
                       rR = rR, rFR = rFR, rPAR = rPAR, 
-                      nblayers = 5, resultingscene = ''):
+                      nblayers = 5, 
+                      discretization = 8, resultingscene = ''):
 
-    par, r, fr, par_sky, r_sky, fr_sky, sc =  compute_zeta_caribu(gapfraction, 
-                      tR, tFR, tPAR, rR, rFR, rPAR, nblayers, resultingscene)
-    plot_zeta_analysis(par, r, fr, par_sky, r_sky, fr_sky)
-    return sc
+    par, r, fr,  sc =  compute_zeta_caribu(gapfraction, 
+                      tR, tFR, tPAR, rR, rFR, rPAR, nblayers, discretization, resultingscene)
+    plot_zeta_analysis(par, r, fr)
+    return sc, [par, r, fr]
 
 
 def plot_zeta_caribu_simp(gapfraction = 0, 
                       pR = (tR+rR)/2, pFR = (tFR+rFR)/2, pPAR = (tPAR+rPAR)/2,
-                      nblayers = 5, resultingscene = ''):
+                      nblayers = 5, 
+                      discretization = 8, resultingscene = ''):
     return plot_zeta_caribu(gapfraction = gapfraction, 
                       tR = pR, tFR = pFR, tPAR = pPAR,
                       rR = pR, rFR = pFR, rPAR = pPAR, 
-                      nblayers = nblayers, resultingscene = resultingscene)
+                      nblayers = nblayers, 
+                      discretization = discretization, resultingscene = resultingscene)
+
+def plot_zeta_caribu_per_triangles(gapfraction = 0, 
+                                   pR = (tR+rR)/2, pFR = (tFR+rFR)/2, pPAR = (tPAR+rPAR)/2,
+                                   nblayers = 5, 
+                                   discretization = 8, resultingscene = ''):
+    par, r, fr, sc, osc =  compute_zeta_caribu_per_triangles(gapfraction, 
+                      tR = pR, tFR = pFR, tPAR = pPAR,
+                      rR = pR, rFR = pFR, rPAR = pPAR, 
+                      nblayers = nblayers, 
+                      discretization = discretization, resultingscene = resultingscene)
+    plot_zeta_analysis_per_triangles(par, r, fr, osc)
+    return sc, [par, r, fr]
 
 
 if __name__ == '__main__':
